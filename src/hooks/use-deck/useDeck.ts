6e3f06271973365ed5797ac521c655de7bcfa611
jermzblake <jermzblake@gmail.com>
2022-01-01 17:41:35 -0500

    initial commit

---
+++ src/hooks/use-deck/useDeck.ts
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import { playingCard } from '../../common/types/'
+
+export const useDeck = () => {
+  const suits = ["spades", "diamonds", "clubs", "hearts"]
+  const values = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
+  
+  const getDeck = () => {
+    let deck: playingCard[] = []
+
+    for(let i = 0; i < suits.length; i++)
+    {
+      for(let x = 0; x < values.length; x++)
+      {
+        let card = {value: values[x], suit: suits[i]};
+        deck.push(card);
+      }
+    }
+  
+    return deck;
+  }
+
+  const [deck, setDeck] = useState(getDeck())
+
+  const shuffle = (deck: playingCard[]) => {
+    // for 1000 turns
+    // switch the values of two random cards
+    for (let i = 0; i < 1000; i++)
+    {
+      let location1 = Math.floor((Math.random() * deck.length));
+      let location2 = Math.floor((Math.random() * deck.length));
+      let tmp = deck[location1];
+
+      deck[location1] = deck[location2];
+      deck[location2] = tmp;
+    }
+  }
+
+  return {
+    shuffle
+  } as const
+
+}
+
