26d2ddbfdec4355a60988be41753ff6de440954e
jermzblake <jermzblake@gmail.com>
2022-01-02 11:29:17 -0500

    add Home Route

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,14 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components/macro'
 import { Route, Routes } from 'react-router-dom'
 import { theme, GlobalStyle } from 'styles/globalstyles'
+import Home from './pages/dashboard/Home'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
       <Routes>
-        
+        <Route path="/" element={<Home />} />
       </Routes>
     </ThemeProvider>
   );
