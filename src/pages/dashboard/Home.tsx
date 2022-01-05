5ff32bd87cb89072607456102b2630ba62d89298
jermzblake <jermzblake@gmail.com>
2022-01-04 22:34:55 -0500

    add SelectUsername and useLandingPage to Home

--- src/pages/dashboard/Home.tsx
+++ src/pages/dashboard/Home.tsx
@@ -1,13 +1,29 @@
 import React, { FunctionComponent } from 'react'
 import { MainContainer } from 'components/page/containers/MainContainer'
 import { Box, Typography } from '@material-ui/core'
+import { SelectUsername } from './components/SelectUsername'
+import { useLandingPage } from './hooks/useLandingPage'
+
 
 const Home: FunctionComponent = () => {
-
+  const { usernameSelected, setUsernameSelected, submitUsername, username, setUsername } = useLandingPage()
   return (
-    <MainContainer title='Welcome'>
-      <Box>
-        <Typography variant='h1' component='div'>Son Ofa B</Typography>
+    <MainContainer title="Welcome">
+      <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+        <Box>
+          <Typography variant="h1" component="div">
+            Son Ofa B
+          </Typography>
+        </Box>
+        <Box>
+          <SelectUsername
+            usernameSelected={usernameSelected}
+            setUsernameSelected={setUsernameSelected}
+            submitUsername={submitUsername}
+            username={username}
+            setUsername={setUsername}
+          />
+        </Box>
       </Box>
     </MainContainer>
   )
