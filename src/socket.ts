f4bd08950f02db8bd222f5c09508691d0c9499e6
jermzblake <jermzblake@gmail.com>
2022-01-06 23:16:48 -0500

    add env option for URL

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client"
 
-const URL = "http://localhost:3000"
+const URL = process.env.SERVER_URL || "http://localhost:3001"
 const socket = io(URL, { autoConnect: false }) //autoConnect is set to false so the connection is not established right away. We will manually call socket.connect() later, once the user has selected a username
 
 socket.onAny((event, ...args) => {
