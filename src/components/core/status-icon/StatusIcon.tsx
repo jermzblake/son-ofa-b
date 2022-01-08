a52ca41ad658fa08c21acdec61c1e3d130b8f8c1
jermzblake <jermzblake@gmail.com>
2022-01-08 15:19:58 -0500

    create status icon component

---
+++ src/components/core/status-icon/StatusIcon.tsx
@@ -0,0 +1,23 @@
+import React, { FunctionComponent } from 'react'
+import { Box } from '@material-ui/core'
+import styled from 'styled-components'
+
+const StyledIcon = styled(Box)`
+ && {
+  height: 8px;
+  width: 8px;
+  border-radius: 50%;
+  display: inline-block;
+  margin-right: 6px;
+ }
+`
+
+interface StatusIconProps {
+  connected: boolean
+}
+
+export const StatusIcon: FunctionComponent<StatusIconProps> =({connected}) => {
+  return (
+    <StyledIcon style={{backgroundColor: connected ? '#86bb71' : '#e38968'}} />
+  )
+}
