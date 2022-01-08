c3d41156e72782eaab197b1eae5efee52ed17159
jermzblake <jermzblake@gmail.com>
2022-01-08 13:13:25 -0500

    refactor

--- src/common/types/deck.types.ts
+++ src/common/types/deck.types.ts
@@ -1,7 +1,7 @@
 export type suits = 'hearts' | 'spades' | 'clubs' | 'diamonds'
 export type values = 'ace' | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 'jack' | 'queen' | 'king'
 
-export interface playingCard {
+export interface PlayingCard {
   value: string
   suit: string
 } 
