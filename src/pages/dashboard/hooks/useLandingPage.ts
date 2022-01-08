42c9f709430aef1c3b234fa99ce14a97a3512b72
jermzblake <jermzblake@gmail.com>
2022-01-08 13:14:38 -0500

    navigate to lobby from useLandingPage

--- src/pages/dashboard/hooks/useLandingPage.ts
+++ src/pages/dashboard/hooks/useLandingPage.ts
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import socket from '../../../socket'
+import { useNavigate } from 'react-router-dom'
 
 export const useLandingPage = () => {
   const [usernameSelected, setUsernameSelected] = useState(false)
   const [username, setUsername] = useState<string>()
+  const navigate = useNavigate()
   
   useEffect(() => {
     socket.on("connect_error", (err) => {
       if (err.message === "invalid username") {
         setUsernameSelected(false);
+        // Should I handle this low-level error (notify user of connection failure?)
       }
     })
     return () => {
@@ -21,6 +24,7 @@ export const useLandingPage = () => {
     setUsernameSelected(true)
     socket.auth = { username }
     socket.connect()
+    navigate('/lobby')
   }
   
   return {
