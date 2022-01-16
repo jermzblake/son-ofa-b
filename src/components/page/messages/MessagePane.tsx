ba530e5ec2006037d902e56e81a6c06bd4342534
jermzblake <jermzblake@gmail.com>
2022-01-16 15:50:48 -0500

    add form and helper methods to MessagePane

--- src/components/page/messages/MessagePane.tsx
+++ src/components/page/messages/MessagePane.tsx
@@ -1,17 +1,16 @@
-import React, { FunctionComponent } from 'react'
-import { MainContainer } from 'components/page/containers/MainContainer'
-import { Box, Typography, Divider } from '@material-ui/core'
+import React, { FunctionComponent, useState } from 'react'
+import { Box, Typography, Divider, TextField, Button } from '@material-ui/core'
 import styled from 'styled-components'
 import { User } from 'common/types'
 import { useTheme } from 'styled-components'
 
 const MessagesWrapper = styled(Box)`
   && {
-    position: fixed;
-    bottom: 0;
+    /* position: fixed;
+    bottom: 0; */
     color: ${props => props.theme.colors.primary};
     background-color: white;
-    width: 60%;
+    /* width: 60%; */
     border-radius: 5px;
     margin-top: 1em;
   }
@@ -19,21 +18,38 @@ const MessagesWrapper = styled(Box)`
 
 interface MessagePanelProps {
   user?: User
-
+  sendMessage
 }
 
-export const MessagePane: FunctionComponent<MessagePanelProps> = ({ user }) => {
+export const MessagePane: FunctionComponent<MessagePanelProps> = ({ user, sendMessage }) => {
   const theme = useTheme()
+  const [message, setMessage] = useState<string>()
+
+  const handleChange = async (e) => {
+    e.preventDefault()
+    setMessage(e.target.value)
+  }
+
+  const isValid = () => {
+    return message?.length > 0
+  }
 
   return (
     <MessagesWrapper>
       <Box>
-        <Typography variant='h4'>Messages</Typography>
+        <Typography variant='h4'>{user.username} Messages</Typography>
       </Box>
       <Divider />
       <Box display='flex' flexDirection='column'>
         <Box color={theme.colors.lightText} ><Typography variant='caption'>Sender's name will go here</Typography></Box>
         <Box>messages will go here with sender's name above</Box>
+      </Box>
+      <Divider />
+      <Box>
+        <form autoComplete='off' onSubmit={sendMessage}>
+          <TextField label='Send message' onChange={e => handleChange(e)} placeholder='Type message...' />
+          <Button onClick={() => sendMessage(message)} type='submit' variant='contained' color="primary" disabled={!isValid()}>Send</Button>
+        </form>
       </Box>
     </MessagesWrapper>
   )
