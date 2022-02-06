9fb1e9bfdb4b231eada90c2472c448779559530b
jermzblake <jermzblake@gmail.com>
2022-02-06 17:02:00 -0500

    add  ThemeManager to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components/macro'
 import { Route, Routes } from 'react-router-dom'
 import { theme, GlobalStyle } from 'styles/globalstyles'
+import { ThemeManager } from 'hooks/theme'
 import Home from './pages/dashboard/Home'
 import Lobby from './pages/lobby/Lobby'
 
@@ -9,10 +10,12 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <ThemeManager>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/lobby" element={<Lobby />} />
       </Routes>
+      </ThemeManager>
     </ThemeProvider>
   );
 }
