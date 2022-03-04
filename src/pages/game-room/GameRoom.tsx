8797b8be348f7f1942eeed31812e7f08afc9610a
jermzblake <jermzblake@gmail.com>
2022-03-03 21:41:01 -0500

    import useGame into GameRoom

--- src/pages/game-room/GameRoom.tsx
+++ src/pages/game-room/GameRoom.tsx
@@ -2,13 +2,15 @@ import React, { FunctionComponent } from 'react'
 import { Box, Typography } from '@material-ui/core'
 import styled from 'styled-components'
 import { MainContainer } from 'components/page/containers/MainContainer'
+import { useGame } from './hooks/useGame'
 
 
 const GameRoom: FunctionComponent = () => {
+  const { currentGame, setCurrentGame, users, messages } = useGame()
 
   return (
     <MainContainer title='Game Room'>
-      <Box><Typography variant='h1'>GAME ROOM</Typography></Box>
+      <Box><Typography variant='h1'>GAME ROOM {currentGame?.id}</Typography></Box>
     </MainContainer>
   )
 }
