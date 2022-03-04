fa72398bac50b941eefd98642bf40134e7a8efdf
jermzblake <jermzblake@gmail.com>
2022-03-03 20:43:34 -0500

    add show game controller

--- controllers/games.ts
+++ controllers/games.ts
@@ -1,8 +1,9 @@
 import { GameModel } from '../models/game'
+import mongoose from 'mongoose'
 
 export const create = async (req, res) => {
-  let game = await new GameModel(req.body)
-  game.save((err) => {
+  const game = await new GameModel(req.body)
+  game.save(err => {
     if (err) {
       console.log(err)
       return res.json(err)
@@ -12,10 +13,21 @@ export const create = async (req, res) => {
 }
 
 export const show = async (req, res) => {
+  try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) return res.status(400).json({ msg: 'Game not found' })
 
+    const game = await GameModel.findById(req.params.id).exec() // find one with matching gameId, otherwise 'null'
+    if (!game) {
+      return res.status(400).json({ msg: 'Game not found' })
+    }
+    return res.json(game)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
 }
 
 export const index = async (req, res) => {
-  let games = await GameModel.find()
+  const games = await GameModel.find()
   return res.json(games)
 }
