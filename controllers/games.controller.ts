c2d0792f8b7e13fccc3554c9f883c456d4cdfef0
jermzblake <jermzblake@gmail.com>
2022-03-07 21:21:37 -0500

    add addPlayerToGame controller method and route

--- controllers/games.controller.ts
+++ controllers/games.controller.ts
@@ -53,3 +53,28 @@ export const update = async (req, res) => {
     res.status(500).send('Server Error')
   }
 }
+
+export const addPlayerToGame = async (req, res) => {
+  try {
+    // Parameter is checked for valid ObjectId before
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) return res.status(400).json({ msg: 'Game not found' })
+
+    const game = await GameModel.findById(req.params.id).exec() // find one with matching gameId, otherwise 'null'
+    if (!game) {
+      return res.status(400).json({ msg: 'Game not found' })
+    }
+    if (game.players?.length >= game.playerCount) {
+      throw new Error(`Game is full`)
+    }
+    game.players.push(req.params.userId)
+    console.log('game pre update', game)
+    const updatedGame = await GameModel.findByIdAndUpdate(req.params.id, game, { new: true })
+    console.log('updated game', updatedGame)
+    updatedGame.id = updatedGame._id
+    delete updatedGame._id
+    return res.json(updatedGame)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+}
--- routes/api/games.routes.ts
+++ routes/api/games.routes.ts
@@ -6,5 +6,5 @@ router.get('/', gameCtrl.index)
 router.get('/:id', gameCtrl.show)
 router.post('/', gameCtrl.create)
 router.put('/:id', gameCtrl.update)
-
+router.put('/:id/user/:userId', gameCtrl.addPlayerToGame)
 export default router
