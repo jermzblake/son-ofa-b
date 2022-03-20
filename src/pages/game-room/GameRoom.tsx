dd5a27f5ff13ca875571914fe74742084c8387fd
jermzblake <jermzblake@gmail.com>
2022-03-20 17:18:04 -0400

    add PreGameModal to GameRoom

--- src/pages/game-room/GameRoom.tsx
+++ src/pages/game-room/GameRoom.tsx
@@ -1,16 +1,38 @@
 import React, { FunctionComponent } from 'react'
-import { Box, Typography } from '@material-ui/core'
+import { Box, Button, Typography } from '@material-ui/core'
 import styled from 'styled-components'
 import { MainContainer } from 'components/page/containers/MainContainer'
 import { useGame } from './hooks/useGame'
-
+import { PreGameModal } from './components/PreGameModal'
 
 const GameRoom: FunctionComponent = () => {
-  const { currentGame, setCurrentGame, users, messages } = useGame()
+  const {
+    currentGame,
+    setCurrentGame,
+    backendPlayer,
+    messages,
+    readyUp,
+    showPreGame,
+    setShowPreGame,
+    startGame,
+    checkPlayersAreReady
+  } = useGame()
 
   return (
-    <MainContainer title='Game Room'>
-      <Box><Typography variant='h1'>GAME ROOM {currentGame?.id}</Typography></Box>
+    <MainContainer title="Game Room">
+      <Box>
+        <Typography variant="h1">GAME ROOM</Typography>
+      </Box>
+
+      <PreGameModal
+        readyPlayer={readyUp}
+        showPreGame={showPreGame}
+        setShowPreGame={setShowPreGame}
+        players={currentGame?.players}
+        backendPlayer={backendPlayer}
+        startGame={startGame}
+        checkPlayersAreReady={checkPlayersAreReady}
+      />
     </MainContainer>
   )
 }
