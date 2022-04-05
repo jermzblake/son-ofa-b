c67aac71f2c72ef9a1812a6f9261ed4580ae83df
jermzblake <jermzblake@gmail.com>
2022-04-05 08:34:24 -0400

    add opponent cards to GameBoard

--- src/pages/game-room/components/GameBoard.tsx
+++ src/pages/game-room/components/GameBoard.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 import { Game, Player } from 'common/types'
 import { GameCard } from './GameCard'
 import { PlayerHUD } from './PlayerHUD'
+import { Opponent } from './Opponent'
 
 interface GameBoardProps {
   game: Game
@@ -13,19 +14,34 @@ interface GameBoardProps {
 }
 
 const StyledGameCardWrapper = styled(Box)`
-  width: 8em;
+  width: 5.2em;
   border: 1px solid yellow;
+  background-color: yellow;
 `
 
 export const GameBoard: FunctionComponent<GameBoardProps> = ({ game, backendPlayer, bidsIn, submitPlayerBid }) => {
 
   return (
-    <Box  >
+    <Box  width="100%">
+      <Box width="100%" display="flex">
+        {game?.enabled && backendPlayer && (
+          game?.players?.map((player, idx) => {
+            if (player.id !== backendPlayer.id) {
+              // TODO depending on players length render the items differently.
+              // idx 2 and 3 can both be middle
+              // Use grid
+              return (
+                <Opponent player={player} key={idx + player.id} />
+              )
+            }
+          })
+        )}
+      </Box>
       {/* give trumpSuit a border or some sort of highlight */}
       <Box>
       {game?.trumpSuit && (
         <StyledGameCardWrapper>
-          <GameCard playingCard={game?.trumpSuit} />
+          <GameCard playingCard={game?.trumpSuit} trump />
         </StyledGameCardWrapper>
       )}
       </Box>
