a0f21268ef2b0d3273f6b1f490f960654d8846fe
jermzblake <jermzblake@gmail.com>
2022-04-08 08:52:24 -0400

    add game pile and containers for Opponent and pile

--- src/pages/game-room/components/GameBoard.tsx
+++ src/pages/game-room/components/GameBoard.tsx
@@ -15,15 +15,16 @@ interface GameBoardProps {
 
 const StyledGameCardWrapper = styled(Box)`
   width: 5.2em;
-  border: 1px solid yellow;
-  background-color: yellow;
+  border: 1px solid ${props => props.theme.colors.third};
+  background-color: ${props => props.theme.colors.third};
 `
 
 export const GameBoard: FunctionComponent<GameBoardProps> = ({ game, backendPlayer, bidsIn, submitPlayerBid }) => {
 
   return (
     <Box  width="100%">
-      <Box width="100%" display="flex">
+      <Box width="100%" display="flex" flexDirection='column'>
+        <Box display='flex'>
         {game?.enabled && backendPlayer && (
           game?.players?.map((player, idx) => {
             if (player.id !== backendPlayer.id) {
@@ -36,6 +37,21 @@ export const GameBoard: FunctionComponent<GameBoardProps> = ({ game, backendPlay
             }
           })
         )}
+        </Box>
+        <Box display='flex'>
+        {bidsIn && (
+          game?.pile?.map((card, idx) => {
+            /** 
+             * TODO: need to turn this into a grid layout 
+             * columns will be based on game.players length
+             * ? how can i tie this to the player that played the card?
+             * */ 
+            return (
+              <GameCard playingCard={card} />
+            )
+          })
+        )}
+        </Box>
       </Box>
       {/* give trumpSuit a border or some sort of highlight */}
       <Box>
