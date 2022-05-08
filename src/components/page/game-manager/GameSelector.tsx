a1fe627012c8aaa3db94e6940dbc268e140075c0
jermzblake <jermzblake@gmail.com>
2022-05-08 17:18:35 -0400

    add table to GameSelector

--- src/components/page/game-manager/GameSelector.tsx
+++ src/components/page/game-manager/GameSelector.tsx
@@ -1,13 +1,33 @@
 import React, { FunctionComponent } from 'react'
-import  { Box, Button, Typography } from '@material-ui/core'
+import {
+  Box,
+  Button,
+  Typography,
+  Table,
+  TableContainer,
+  TableCell,
+  TableRow,
+  TableHead,
+  TableBody
+} from '@material-ui/core'
 import styled from 'styled-components'
 import { Game } from 'common/types'
 import { useNavigate } from 'react-router-dom'
+import { useTheme } from 'styled-components'
 
-const GameList = styled(Box)`
+const StyledGameListBox = styled(Box)`
   && {
     display: flex;
     flex-direction: column;
+  }
+`
+
+const StyledTableContainer = styled(TableContainer)`
+  && {
+    max-height: 28em;
+    overflow-y: auto;
+    scrollbar-width: none;
+    overflow-x: hidden;
   }
 `
 
@@ -18,24 +38,85 @@ export interface GameSelectorProps {
 
 export const GameSelector: FunctionComponent<GameSelectorProps> = ({ currentGames, setShowCreateGame }) => {
   const navigate = useNavigate()
-  
+  const theme = useTheme()
+
   return (
     <Box>
-      <Typography variant="h5"><Box>Select Game</Box></Typography>
-      <Typography variant='caption'><Box>Join an open game or create a new game</Box></Typography>
-      {currentGames && currentGames.length > 0  && (
-        <GameList>
-          {currentGames.map((game, index) => {
-            return (
-              <Box key={game.id + index} onClick={() => ''}>
-                <Typography component='span'><Box>{game.name} - {game.playerCount}</Box></Typography>
-                <Button color="secondary" onClick={() => navigate(`/game/${game.id}`)}>Join Game</Button>
-              </Box>
-            )
-          })}
-        </GameList>
+      <Typography variant="h5">
+        <Box>Select Game</Box>
+      </Typography>
+      <Typography variant="caption">
+        <Box>Join an open game or create a new game</Box>
+      </Typography>
+      {currentGames && currentGames.length > 0 && (
+        <StyledTableContainer>
+          <Table stickyHeader aria-label="game list" style={{ backgroundColor: theme.colors.backgroundComplementII }}>
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">
+                  <Typography variant="body1">
+                    <Box fontWeight="bold">Creator</Box>
+                  </Typography>
+                </TableCell>
+                <TableCell align="center">
+                  <Typography variant="body1">
+                    <Box fontWeight="bold">Players</Box>
+                  </Typography>
+                </TableCell>
+                <TableCell align="center">
+                  <Typography variant="body1">
+                    <Box fontWeight="bold">Rounds</Box>
+                  </Typography>
+                </TableCell>
+                <TableCell align="center">
+                  <Typography variant="body1">
+                    <Box fontWeight="bold"> </Box>
+                  </Typography>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {currentGames.map((game, index) => {
+                return (
+                  <TableRow key={game.id + index}>
+                    <TableCell>
+                      <Typography variant="body1">
+                        <Box fontWeight="bold" color={theme.colors.baseText}>
+                          {game.name}
+                        </Box>
+                      </Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography variant="body1">
+                        <Box fontWeight="bold" color={theme.colors.baseText}>
+                          {game.playerCount}
+                        </Box>
+                      </Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography variant="body1">
+                        <Box fontWeight="bold" color={theme.colors.baseText}>
+                          {game.rounds}
+                        </Box>
+                      </Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Button color="secondary" onClick={() => navigate(`/game/${game.id}`)}>
+                        Join Game
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                )
+              })}
+            </TableBody>
+          </Table>
+        </StyledTableContainer>
       )}
-      <Button onClick={() => setShowCreateGame(true)}>Create Game</Button>
+      <Box display="flex" justifyContent="flex-end" width="100%" mt="1em">
+        <Button variant="contained" onClick={() => setShowCreateGame(true)}>
+          Create Game
+        </Button>
+      </Box>
     </Box>
   )
 }
