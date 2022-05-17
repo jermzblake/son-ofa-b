7bc7b8bb9a0d5e7a537b70e2473d1a509a6028da
jermzblake <jermzblake@gmail.com>
2022-05-17 08:14:05 -0400

    update path to build again

--- server.ts
+++ server.ts
@@ -22,8 +22,8 @@ app.use(express.urlencoded({ extended: true }));
 
 // Configure both serve-favicon & static middlewares
 // to serve from the production 'build' folder but for now serving out of public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')) as any);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')) as any);
+app.use(express.static(path.join(__dirname, 'build')));
 
 // Put API routes here, before the "catch all" route
 
