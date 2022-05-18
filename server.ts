c28b870c30d7bbc1f6428a6f87551db00b08ae77
jermzblake <jermzblake@gmail.com>
2022-05-18 08:26:24 -0400

    change directory from build to public for serve-favicon

--- server.ts
+++ server.ts
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Configure both serve-favicon & static middlewares
 // to serve from the production 'build' folder but for now serving out of public
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')) as any);
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')) as any);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Put API routes here, before the "catch all" route
