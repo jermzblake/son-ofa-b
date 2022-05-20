8c4d54260016e9822f46b283ce423dc273dc17b0
jermzblake <jermzblake@gmail.com>
2022-05-20 08:48:00 -0400

    add basename prop to BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router><App /></Router>
+    <Router basename='/'><App /></Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
