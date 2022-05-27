51ccce6971265aeeccef297fa1c673c991e273b4
jermzblake <jermzblake@gmail.com>
2022-05-26 20:41:34 -0400

    remove basename from router

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename='/index.html'><App /></Router>
+    <Router><App /></Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
