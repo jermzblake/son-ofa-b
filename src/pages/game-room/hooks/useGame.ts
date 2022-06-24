46562c0292492cba79b0ff0b41bac7d6967862d9
jermzblake <jermzblake@gmail.com>
2022-06-23 21:53:50 -0400

    refactor

--- src/pages/game-room/hooks/useGame.ts
+++ src/pages/game-room/hooks/useGame.ts
@@ -16,7 +16,7 @@ export const useGame = () => {
   const [users, setUsers] = useState<User[]>([])
   const [messages, setMessages] = useState<MessageBoard[]>([])
   const [, updateState] = useState()
-    // @ts-ignore
+  // @ts-ignore
   const forceUpdate = useCallback(() => updateState({}), [])
   const [backendPlayer, setBackendPlayer] = useState<Player>()
   const { shuffle, getDeck } = useDeck()
@@ -27,84 +27,81 @@ export const useGame = () => {
   const [showChat, setShowChat] = useState(false)
   const navigate = useNavigate()
 
-
-  const getData = async() => {
+  const getData = async () => {
     try {
-    const game: Game = await getGame(gameId)
-    if (game) {
-      setCurrentGame(game)
-      if (game.enabled) {
-        setShowPreGame(false)
-        const playerIndex = game.players?.findIndex(player => player.id === (socket as any).userId)
-        if (playerIndex > -1) {
-        setBackendPlayer(game.players[playerIndex])
-        socket.emit('player reconnected', {game , playerIndex})
-        }
-        if (game.players.findIndex(player => player.bid === null || undefined) < 0) {
-          setBidsIn(true)
+      const game: Game = await getGame(gameId)
+      if (game) {
+        setCurrentGame(game)
+        if (game.enabled) {
+          setShowPreGame(false)
+          const playerIndex = game.players?.findIndex(player => player.id === (socket as any).userId)
+          if (playerIndex > -1) {
+            setBackendPlayer(game.players[playerIndex])
+            socket.emit('player reconnected', { game, playerIndex })
+          }
+          if (game.players.findIndex(player => player.bid === null || undefined) < 0) {
+            setBidsIn(true)
+          } else {
+            setBidsIn(false)
+          }
         } else {
-          setBidsIn(false)
+          socket.emit('player connected', { game })
         }
       } else {
-      socket.emit('player connected', {game})
+        navigate('*')
       }
+    } catch (error) {
+      if (error.message.includes('40')) navigate('*')
     }
-    else {
-      navigate('*')
-    }
-  } catch (error) {
-    
-    if (error.message.includes('40')) navigate('*')
-  }
   }
 
   useEffect(() => {
-    const sessionId = getItem("sessionId")
+    const sessionId = getItem('sessionId')
 
     if (sessionId) {
       socket.auth = { sessionId }
       socket.connect()
     }
 
-    socket.on("session", ({ sessionId, userId }) => {
+    socket.on('session', ({ sessionId, userId }) => {
       // attach the session ID to the next reconnection attempts
       socket.auth = { sessionId }
       // store it in the localStorage
-      setItem("sessionId", sessionId);
+      setItem('sessionId', sessionId)
       // save the ID of the user
-      (socket as any).userId = userId
+      ;(socket as any).userId = userId
     })
 
-    socket.on("connect", () => {
-      users.forEach((user) => {
+    socket.on('connect', () => {
+      users.forEach(user => {
         if (user) {
-          user.connected = true;
+          user.connected = true
         }
       })
     })
 
-    socket.on("disconnect", () => {
-      users.forEach((user) => {
+    socket.on('disconnect', () => {
+      users.forEach(user => {
         if (user) {
-          user.connected = false;
+          user.connected = false
         }
       })
     })
 
     const initReactiveProperties = (user: User) => {
-      user.connected = true;
-      user.messages = [];
-      user.hasNewMessages = false;
+      user.connected = true
+      user.messages = []
+      user.hasNewMessages = false
     }
-  
-    socket.on("user connected", (user: User) => {
+
+    socket.on('user connected', (user: User) => {
       initReactiveProperties(user)
       users.push(user)
       setUsers(users)
     })
 
-    socket.on("player joined", async (game) => {
-      const updatedGame = await addPlayerToGame(game.game.id,{
+    socket.on('player joined', async game => {
+      const updatedGame = await addPlayerToGame(game.game.id, {
         id: game.user.userId,
         gamertag: game.user.username,
         hand: [],
@@ -133,13 +130,13 @@ export const useGame = () => {
       }
     })
 
-    socket.on("player rejoined", (player) => {
+    socket.on('player rejoined', player => {
       setBackendPlayer(player)
       forceUpdate()
     })
 
-    socket.on("game updated", (game) => {
-      if (game.winner){
+    socket.on('game updated', game => {
+      if (game.winner) {
         // do winner work
         toast(`${game.winner} has won!!`, { duration: 8000, icon: '🔥' })
       }
@@ -168,68 +165,72 @@ export const useGame = () => {
       forceUpdate()
     })
 
-    socket.on("group message", (message) => {
-      messages.push({content: message.content, sender: message.from})
+    socket.on('group message', message => {
+      messages.push({ content: message.content, sender: message.from })
       setMessages([...messages])
     })
-  
-    socket.on("user disconnected", (id) => {
+
+    socket.on('user disconnected', id => {
       for (let i = 0; i < users.length; i++) {
-        const user = users[i];
-  
+        const user = users[i]
+
         if (user.userId === id) {
-          user.connected = false;
+          user.connected = false
           forceUpdate()
-          break;
+          break
         }
       }
     })
-    getData() 
+    getData()
 
     return () => {
-      socket.off("connect")
-      socket.off("disconnect")
-      socket.off("users")
-      socket.off("user connected")
-      socket.off("user disconnected")
-      socket.off("group message")
-      socket.off("player joined")
-      socket.off("game updated")
-      socket.off("player rejoined")
+      socket.off('connect')
+      socket.off('disconnect')
+      socket.off('users')
+      socket.off('user connected')
+      socket.off('user disconnected')
+      socket.off('group message')
+      socket.off('player joined')
+      socket.off('game updated')
+      socket.off('player rejoined')
     }
   }, [])
 
   const startGame = async () => {
     const newDeck = shuffle(getDeck())
-    await startBackendGame(currentGame?.id, {...currentGame, deck: newDeck, enabled: true})
+    await startBackendGame(currentGame?.id, { ...currentGame, deck: newDeck, enabled: true })
   }
 
   const readyUp = async () => {
-    const response = await readyPlayer(currentGame?.id, {...backendPlayer, ready: true})
+    const response = await readyPlayer(currentGame?.id, { ...backendPlayer, ready: true })
   }
 
   const checkPlayersAreReady = () => {
     let howManyReady = 0
     currentGame?.players?.forEach(player => {
-      if (player.ready) howManyReady++ 
+      if (player.ready) howManyReady++
     })
     return howManyReady === currentGame?.playerCount
   }
 
   const submitPlayerBid = async (bid: number) => {
-    const response = await submitBid(currentGame?.id, {...backendPlayer, bid: bid})
+    const response = await submitBid(currentGame?.id, { ...backendPlayer, bid: bid })
   }
 
   const handleCardSelect = async (card: PlayingCard) => {
-      // check that this card can be played (suit match player has card with trump suit)
-      // send notification if card is wrong suit
+    // check that this card can be played (suit match player has card with trump suit)
+    // send notification if card is wrong suit
     if (currentGame?.pile?.length > 0 && !checkCardIsPlayable(backendPlayer?.hand, card, currentGame?.leadSuit)) {
       setSelectedCard(null)
       return toast.error('Player must play card with suit matching leading suit')
     }
-    if (card.suit === selectedCard?.suit && card.value === selectedCard?.value){
-      const removedCardHand = backendPlayer?.hand.filter(playerCard => playerCard.suit !== card.suit ||  playerCard.value !== card.value)
-      const response = backendPlayer?.dealer ? await takeDealerTurn(currentGame?.id, {...backendPlayer, hand: removedCardHand}, selectedCard) : await takeTurn(currentGame?.id, {...backendPlayer, hand: removedCardHand}, selectedCard)
+    if (card.suit === selectedCard?.suit && card.value === selectedCard?.value) {
+      const removedCardHand = backendPlayer?.hand.filter(
+        playerCard => playerCard.suit !== card.suit || playerCard.value !== card.value
+      )
+      const response = backendPlayer?.dealer
+        ? await takeDealerTurn(currentGame?.id, { ...backendPlayer, hand: removedCardHand }, selectedCard)
+        : await takeTurn(currentGame?.id, { ...backendPlayer, hand: removedCardHand }, selectedCard)
       if (response) {
         setSelectedCard(null)
       }
@@ -240,7 +241,7 @@ export const useGame = () => {
 
   const sendMessage = (e, content) => {
     e.preventDefault()
-    socket.emit('group message', {content, sender: backendPlayer?.gamertag, room: currentGame?.id})
+    socket.emit('group message', { content, sender: backendPlayer?.gamertag, room: currentGame?.id })
   }
 
   return {
