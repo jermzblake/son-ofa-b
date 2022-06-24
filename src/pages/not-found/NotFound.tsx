7bc29d94d0b4ee99f4fde4fb599e9f6ef1db43e7
jermzblake <jermzblake@gmail.com>
2022-06-23 21:51:30 -0400

    add go home link

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -4,35 +4,45 @@ import { MainContainer } from 'components/page/containers/MainContainer'
 import { useNavigate } from 'react-router-dom'
 import ExitToAppIcon from '@material-ui/icons/ExitToApp'
 import styled from 'styled-components'
+import { useTheme } from 'styled-components'
 
 const StyledExitIcon = styled(ExitToAppIcon)`
   transform: scale(4);
+  color: ${props => props.theme.colors.secondary};
+  margin: 1em 0;
+`
+
+const StyledBox = styled(Box)`
+  width: 110%;
+  background-color: ${props => props.theme.colors.base};
+  margin: -1em;
 `
 
 export const NotFound: FunctionComponent = () => {
   const navigate = useNavigate()
+  const theme = useTheme()
+
   return (
     <MainContainer title="whoops...">
-      <Box margin='0 auto'>
-        <Typography variant='h1'>
-          <Box>
-            404
-          </Box>
+      <StyledBox display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+        <Typography variant="h1" color="secondary">
+          404
         </Typography>
-        <Typography variant='h3'>
-          <Box>
-            Page Not Found
-          </Box>
+        <Typography variant="h3">Page Not Found</Typography>
+        <Typography variant="h5" gutterBottom>
+          Whoops! Looks like we've wandered a little too far.
         </Typography>
-        <Typography variant='h4'>
-          <Box>
-            Whoops! Looks like we've wandered a little too far.
-          </Box>
-        </Typography>
-        <IconButton onClick={() => navigate(-1)}>
-          <StyledExitIcon />
-        </IconButton>
-      </Box>
+        <Box display="flex">
+          <IconButton onClick={() => navigate(-1)}>
+            <StyledExitIcon />
+          </IconButton>
+        </Box>
+        <Box position="fixed" bottom="0" right="3em" onClick={() => navigate('/')} color={theme.colors.third}>
+          <Typography variant="body1">
+            <em>Go Home</em>
+          </Typography>
+        </Box>
+      </StyledBox>
     </MainContainer>
   )
 }
